39778cc214ea8b996ea36f1e665e3cfed8504825
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-06-03 11:56:03 +0530

    Modified home to use sidebar.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,13 @@
 import {Header} from '../header';
+import {SideBar} from '../sidebar';
+import {SideBarContent} from '../sidebar-content';
 
 export function Home(){
-return <div><Header/></div>
+return (<div className="home"><Header/>
+<div className="home-content">
+<SideBar/>
+<SideBarContent/>
+</div>
+</div>
+)
 }

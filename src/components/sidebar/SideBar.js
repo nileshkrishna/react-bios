e57cc5021db39a9d5ea0dd699c2e9882d42122c5
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-06-04 13:12:06 +0530

    Added sidebar options.

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,3 +1,24 @@
+const links=[{name:'home', display:'HOME'},
+{name:'new-archival-initiation', display:'NEW ARCHIVAL INITIATION'},
+{name:'contact-us', display:'CONTACT US'}];
+
 export function SideBar(){
-return <div className="sidebar">Sidebar</div>
+return (
+<div className="sidebar">
+<div className="header">
+AIDE
+</div>
+
+{
+links.map((l,index)=>(<LinkItem item={l}/>))
+}
+
+</div>
+)
+}
+
+function LinkItem({item}){
+return (
+<div className="link-item">{item.display}</div>
+)
 }

f1918892103f951c9aba791f1bcd9751c1e31e8c
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-06-04 13:34:38 +0530

    Changes to highlight selected item

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,8 +1,17 @@
+import {useCallback, useState} from 'react';
+
 const links=[{name:'home', display:'HOME'},
 {name:'new-archival-initiation', display:'NEW ARCHIVAL INITIATION'},
 {name:'contact-us', display:'CONTACT US'}];
 
 export function SideBar(){
+
+const [selectedIndex, setSelectedIndex]=useState(0);
+
+const handleClick=useCallback((index)=>{
+setSelectedIndex(index);
+},[]);
+
 return (
 <div className="sidebar">
 <div className="header">
@@ -10,15 +19,22 @@ AIDE
 </div>
 
 {
-links.map((l,index)=>(<LinkItem item={l}/>))
+links.map((l,index)=>(<LinkItem item={l} index={index} onClick={handleClick} isSelected={index===selectedIndex}/>))
 }
 
 </div>
 )
 }
 
-function LinkItem({item}){
+function LinkItem({item, index, onClick, isSelected}){
+
+const handleClick= useCallback(()=>{
+onClick(index);
+}, [index, onClick]);
+
+const className=isSelected? "link-item selected": "link-item";
+
 return (
-<div className="link-item">{item.display}</div>
+<div className={className} onClick={handleClick}>{item.display}</div>
 )
 }
